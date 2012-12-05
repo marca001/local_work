893ac148b9612f2bef58ea2399e49f76525370e0
Myca <marca001@ucr.edu>
2012-12-05 13:52:30 -0800

    Changed names, wrote a test script

--- oncall.pl
+++ oncall.pl
@@ -1,43 +1,81 @@
 #!/usr/bin/perl
-#use strict;
-#use warnings;
-
+use IO::File;
 use Data::ICal;
-use Data::Dumper;
-use Date::Simple::D8;
-use Email::MIME;
+use Date::Simple;
+use iCal::Parser;
 use LWP::Simple;
+use Data::Dumper;
+
+###############################################################
+# CIS On Call iCalendar Check
+#	This script returns name of the person on call in calendar
+###############################################################
+
+# Synopsis
+
+# - Write a copy of ical to a file
+# - Create a calendar hash by parsing ical file
+# - Compare events in calendar hash with today's date
+# - If an event matches today's date and summary is "CIS On Call", 
+# - return the name in that event
+
 ###
-#Pull copy of calendar from online
-$file = 'https://www.google.com/calendar/ical/mdsa99%40gmail.com/public/basic.ics';
-#$file = 'https://www.google.com/calendar/ical/ucr.edu_oco3j655u9fflpeqgv7t4pe82o%40group.calendar.google.com/private-ed0fcfa22b88ab9fdc023d2d9de4ac25/basic.ics';
-#$file = 'http://www.google.com/calendar/ical/ucr.edu_oco3j655u9fflpeqgv7t4pe82o%40group.calendar.google.com/public/basic.ics';
+# - Write a copy of ical to a file
+###
+
+$file = 'https://www.google.com/calendar/ical/ucr.edu_oco3j655u9fflpeqgv7t4pe82o%40group.calendar.google.com/private-ed0fcfa22b88ab9fdc023d2d9de4ac25/basic.ics';
 $text = get $file;
-$calendar = Data::ICal->new(data => $text);
+$newfile = 'testcal.ics';
+getstore($file,$newfile);
+$calendar = Data::ICal->new(filename => $newfile);
 
+#If there is trouble downloading the file, end script
 if(!$calendar){
-	print "Calendar not found";
-} 
+	print "Calendar not found.";
+	exit 1;
+}
 
-$events = $calendar->entries;
-$today = Date::Simple::d8($ARGV[0]);
+###
+# - Create a calendar hash by parsing ical file
+###
+
+#Only populate calendar hash with dates from last week to next week
 #$today = Date::Simple->new();
+$today = Date::Simple::d8($ARGV[0]);
+$lastWeek = $today - 7;
+$nextWeek = $today + 7; #maybe we don't need next week...
 
-foreach $entry(@$events){	
 
-	$summaryProperty = $entry->property('summary');
-	$summary = $summaryProperty->[0]->{'value'};
-	@summarySplit = split('-',$summary);
+#Parse ical file to get calendar hash
+$fh = IO::File->new("testcal.ics","r");
+$parser=iCal::Parser->new(start => $lastWeek->as_d8, end => $nextWeek->as_d8);
+$hash = $parser->parse($fh);
+$thisCal = $hash->{'events'};
 
-	$dateStartProperty = $entry->property('dtstart');	
-	$dateEndProperty = $entry->property('dtend');
-	
-	$dateStart = Date::Simple::d8($dateStartProperty->[0]->{'value'});
-	$dateEnd = Date::Simple::d8($dateEndProperty->[0]->{'value'});
-	
-	if($dateStart <= "$today" and 
-		$dateEnd > "$today" and
-		$summarySplit[1] eq "CIS OnCall"){
-		print $summarySplit[2]."\n";
+
+#The calendar hash is a hash of hashes. 
+#Iterate through each hash to get events
+foreach $year(values %$thisCal){	
+	foreach $month(values %$year){
+		foreach $day(values %$month){
+			foreach $event(values %$day){				
+				$dateStart = $event->{'DTSTART'}->ymd;
+				$dateEnd = $event->{'DTEND'}->ymd;
+				@summarySplit = split('-',$event->{'SUMMARY'});
+				
+				###
+				# - If an event matches today's date and summary is "CIS On Call", 
+				# - return the name in that event
+				###
+				
+				if($dateStart <= $today and 
+					$dateEnd > $today and
+					$summarySplit[1] eq "CIS OnCall"){
+					#print "(".$event->{'DTSTART'}->ymd." - ".$event->{'DTEND'}->ymd.") ";
+					print $summarySplit[2]."\n";
+				}
+			}
+		}
 	}
 }
+
--- testscript.pl
+++ testscript.pl
@@ -7,8 +7,9 @@ use LWP::Simple;
 use Data::Dumper;
 
 ###############################################################
-# CIS On Call iCalendar Check
-#	This script returns 
+# CIS On Call iCalendar Check Test
+#	This is a modified version of oncall.pl that returns the next
+#	12+ people on call based on the passed in date.
 ###############################################################
 
 # Synopsis
@@ -16,8 +17,8 @@ use Data::Dumper;
 # - Write a copy of ical to a file
 # - Create a calendar hash by parsing ical file
 # - Compare events in calendar hash with today's date
-# - If an event matches today's date and summary is "CIS On Call", 
-# - return the name in that event
+# - If event summary is "CIS On Call", 
+# - store in hash to be printed
 
 ###
 # - Write a copy of ical to a file
@@ -39,16 +40,19 @@ if(!$calendar){
 # - Create a calendar hash by parsing ical file
 ###
 
-#Only populate calendar hash with dates from last week to next week
-$today = Date::Simple->new();
+#Only populate calendar hash with dates from last week to next 12 weeks
+#$today = Date::Simple->new();
+$today = Date::Simple::d8($ARGV[0]);
 $lastWeek = $today - 7;
-$nextWeek = $today + 7; #maybe we don't need next week...
+$nextWeek = $today + 7*12;
+
 
 #Parse ical file to get calendar hash
 $fh = IO::File->new("testcal.ics","r");
 $parser=iCal::Parser->new(start => $lastWeek->as_d8, end => $nextWeek->as_d8);
 $hash = $parser->parse($fh);
 $thisCal = $hash->{'events'};
+
 
 #The calendar hash is a hash of hashes. 
 #Iterate through each hash to get events
@@ -61,21 +65,22 @@ foreach $year(values %$thisCal){
 				@summarySplit = split('-',$event->{'SUMMARY'});
 				
 				###
-				# - If an event matches today's date and summary is "CIS On Call", 
-				# - return the name in that event
+				# - If event summary is "CIS On Call", 
+				# - store in hash to be printed
 				###
 				
-				if($dateStart <= $today and 
-					$dateEnd > $today and
-					$summarySplit[1] eq "CIS OnCall"){
-					print $summarySplit[2]."\n";
+				if($summarySplit[1] eq "CIS OnCall"){
+					$current = @summarySplit[2];
+					$val = "(".$event->{'DTSTART'}->ymd." - ".$event->{'DTEND'}->ymd.") ".$summarySplit[2]."\n";
+					$finalHash{$dateStart} = $val;
 				}
-			
-				 # print $event->{'DTSTART'}->ymd." - "
-					# .$event->{'DTEND'}->ymd.": "
-					# .$event->{'SUMMARY'}."\n";
 			}
 		}
 	}
+}
+
+#Sort hash and print
+foreach $value(sort {$finalHash{$a} cmp $finalHash{$b} } keys %finalHash){
+	print $finalHash{$value};
 }
 
